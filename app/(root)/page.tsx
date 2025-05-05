0fb0eb4c1092b336b928fd1bc1123ca56205422f
JeetDas5 <jeet15083011@gmail.com>
2025-05-05 21:21:57 +0530

    added get interview function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,15 @@ import Link from "next/link";
 import Image from "next/image";
 import { dummyInterviews } from "@/constants";
 import InterviewCard from "@/components/InterviewCard";
+import {
+  getCurrentUser,
+  getInterviewByUserId,
+} from "@/lib/actions/auth.actions";
 
-const page = () => {
+const page = async () => {
+  const user = await getCurrentUser();
+  const userInterviews = await getInterviewByUserId(user?.id!);
+  const hasPastInterviews = userInterviews?.length! > 0;
   return (
     <>
       <section className="card-cta">
@@ -34,9 +41,13 @@ const page = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id}/>
-          ))}
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>You haven&apos;t taken any interviews yet</p>
+          )}
         </div>
       </section>
 
@@ -45,7 +56,7 @@ const page = () => {
         <h2>Take an Interview</h2>
         <div className="interviews-section">
           {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id}/>
+            <InterviewCard {...interview} key={interview.id} />
           ))}
           {/* <p>You haven&apos;t taken any interviews yet</p> */}
         </div>
